80a9705a5c16e471dd21d67bbd263e14bee1d8b6
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-06-07 18:38:15 -0600

    first commit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <script>
-  let companyName = 'Our Mission';
-  let address = 'At TaskMaster, we are dedicated to helping you stay organized, focused, and achieve your goals. We provide a user-friendly platform to manage your tasks effectively, prioritize your work, and make progress towards your objectives. Let us be your companion on your journey to success.';
+  let title = 'Our Mission';
+  let paragraph = 'At TaskMaster, we are dedicated to helping you stay organized, focused, and achieve your goals. We provide a user-friendly platform to manage your tasks effectively, prioritize your work, and make progress towards your objectives. Let us be your companion on your journey to success.';
     let icon = './svg/github.svg';
     let currentYear = new Date().getFullYear();
     
