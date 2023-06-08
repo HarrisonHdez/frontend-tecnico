7da16c295274ae36d495b0e18e6d860ad84eb16d
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-06-07 18:29:45 -0600

    first commit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,15 @@
 <script>
-  let companyName = 'Our Mission';
-  let address = 'At TaskMaster, we are dedicated to helping you stay organized, focused, and achieve your goals. We provide a user-friendly platform to manage your tasks effectively, prioritize your work, and make progress towards your objectives. Let us be your companion on your journey to success.';
+  let title = 'Our Mission';
+  let paragraph = 'At TaskMaster, we are dedicated to helping you stay organized, focused, and achieve your goals. We provide a user-friendly platform to manage your tasks effectively, prioritize your work, and make progress towards your objectives. Let us be your companion on your journey to success.';
     let icon = './svg/github.svg';
     let currentYear = new Date().getFullYear();
 </script>
 
 <footer class="footer" aria-label="Pie de página">
   <div class="footer__content container">
-    <h4 class="footer__title">{companyName}</h4>
+    <h4 class="footer__title">{title}</h4>
     <div class="footer__paragraph-container">
-        <p class="footer__paragraph">{address}</p>
+        <p class="footer__paragraph">{paragraph}</p>
         <img class="footer__icon" src={icon} alt="GitHub Icon"/>
     </div>
     <p class="footer__full-year">© {currentYear}  Fernando Leano. All rights reserved.</p>
